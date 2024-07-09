8d019be22a8fb2628550afe4814eda70d27ce573
Thomas Jaron <thomas.jaron@outlook.de>
2024-07-09 12:34:04 +0200

    adjust edge calculation

--- inputs.py
+++ inputs.py
@@ -11,7 +11,6 @@ from easydict import EasyDict as edict
 
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
 def load_images(fps, opt, mode='RGB', invert_gray=False):
     """Loads a set of images into a tensor from a list of file pointers.
@@ -54,50 +53,41 @@ def compute_histograms(images, device):
     # device = string
     return 0
 
-def compute_derivative_y(images, device):
-    """Compute the y-derivative from a tensor holding images."""
-    # TODO
+def compute_edges(images_tensor, device):
+    """Compute edge image tensors from a grayscale image tensor containing 1 or more images."""
     # Images = tensor of size [B, 3, H, W]
     # device = string
-    return 0
-
-def compute_derivative_x(images, device):
-    """Compute the x-derivative from a tensor holding images."""
-    # TODO
-    # Images = tensor of size [B, 3, H, W]
-    # device = string
-    return 0
-
-def compute_edges(rgb, device):
-    """Compute an edge image from a list of image paths."""
-    #print(rgb.size())
     processed_images = []
-    for image in rgb:
-        image = image.detach().cpu().numpy()
-        image = np.transpose(image, (1, 2, 0))
+    for image in images_tensor:
+        # image preparation
+        if image.is_cuda:
+            image = image.detach().cpu()
+        i = image.numpy()
+        i = np.transpose(i, (1, 2, 0))
+        # compute derivatives and edge image
+        sobel_x = cv2.Sobel(i, cv2.CV_64F, 1, 0, ksize=3)
+        sobel_y = cv2.Sobel(i, cv2.CV_64F, 0, 1, ksize=3)
+        i = np.sqrt(sobel_x**2 + sobel_y**2)
+        # blur result to make loss calculation and gradient descent easier
+        i = cv2.GaussianBlur(i, (5, 5), 0)
+        i = torchvision_F.to_tensor(i).to(device)
+        processed_images.append(i)
+    return torch.stack(processed_images)
 
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image_tensor = torchvision_F.to_tensor(image).to(device)
-
-        if image_tensor.is_cuda:
-            image_np = image_tensor.detach().cpu().numpy()
-        else:
-            image_np = image_tensor.detach().numpy()
-
-        image_gray = cv2.cvtColor(np.transpose(image_np, (1, 2, 0)), cv2.COLOR_RGB2GRAY)
-        sobel_x = cv2.Sobel(image_gray, cv2.CV_64F, 1, 0, ksize=3)
-        sobel_y = cv2.Sobel(image_gray, cv2.CV_64F, 0, 1, ksize=3)
-        sobel = np.sqrt(sobel_x**2 + sobel_y**2)
-        sobel_blurred = cv2.GaussianBlur(sobel, (5, 5), 0)
-        sobel_blurred_tensor = torchvision_F.to_tensor(sobel_blurred).to(device)
-        processed_images.append(sobel_blurred_tensor)
-
-        # plt.figure()
-        # plt.imshow(sobel_blurred, cmap='gray')
-        # plt.title('Sobel')
-        # plt.axis('off')
-        # plt.show()
-
+def erode_images(images_tensor, device, kernel=(5, 5)):
+    """Compute eroded grayscale image tensor containing 1 or more images."""
+    # Images = tensor of size [B, x, H, W]
+    # device = string
+    processed_images = []
+    for image in images_tensor:
+        # image preparation
+        if image.is_cuda:
+            image = image.detach().cpu()
+        i = image.numpy()
+        i = np.transpose(i, (1, 2, 0))
+        i = cv2.erode(i, cv2.getStructuringElement(cv2.MORPH_RECT, kernel))
+        i = torchvision_F.to_tensor(i).to(device)
+        processed_images.append(i)
     return torch.stack(processed_images)
 
 
@@ -112,13 +102,14 @@ def prepare_images(opt, fps_images=None, fps_masks=None, fp_gt=None, edges=True)
     inputs.rgb = load_images(fps_images, opt)
     # Invert loaded masks (SIDAR Dataset sets occlusions to 1)
     inputs.masks = load_images(fps_masks, opt, mode='L', invert_gray=True)
+    inputs.masks_eroded = erode_images(inputs.masks, opt.device, kernel=(5,5))
     ##### perform image processing and save the results
     # save grayscale version of images
     inputs.gray = load_images(fps_images, opt, mode='L')
-    inputs.histograms = compute_histograms(inputs.rgb, opt.device)
-    inputs.histograms_normalized = compute_histograms(inputs.rgb, opt.device)
-    inputs.edges = compute_edges(inputs.rgb, opt.device) if edges else None
-    inputs.derivative_x = compute_derivative_x(inputs.rgb, opt.device)
-    inputs.derivative_y = compute_derivative_y(inputs.rgb, opt.device)
+    # generate edge images
+    inputs.edges = compute_edges(inputs.gray, opt.device) if edges else None
+    # TODO:
+    # inputs.histograms = compute_histograms(inputs.rgb, opt.device)
+    # inputs.histograms_normalized = compute_histograms(inputs.rgb, opt.device)
 
     return inputs
